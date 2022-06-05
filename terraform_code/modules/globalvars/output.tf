abda8457e725eba0572a53d87d5fe5c3634be517
lakshayanand14 <lakshayanand97@gmail.com>
2022-06-05 08:55:17 +0000

    firstcommit

---
+++ terraform_code/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Lakshay"
+    "App"   = "Web"
+    "Project" = "CLO835"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "Lab1"
+}
